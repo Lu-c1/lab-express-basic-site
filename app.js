2c82819a5da496884992aba5e3f0d54b490415bb
lucia vilar <vp_lucia@hotmail.com>
2021-07-28 02:44:03 +0200

    solved lab

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//commit test
+const express = require("express")
+const app = express()
+
+app.use(express.static("public"));
+
+app.get("/", (request, response, next) => {
+    response.sendFile(__dirname + "/views/home.html")
+});
+app.get("/about", (request, response, next) => {
+    response.sendFile(__dirname + "/views/about.html")
+});
+
+app.get("/works", (request, response, next) => {
+    response.sendFile(__dirname + "/views/works.html")
+});
+
+app.get("/photo", (request, response, next) => {
+    response.sendFile(__dirname + "/views/photo.html")
+
+});
+
+//localhost: 8000
+app.listen(8000, () => console.log("I´m listening on port 8000"));
